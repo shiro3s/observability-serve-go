cf3d40431ee816c490cc25412a4fcc9f3e5849f8
clownegg <shun-0405-uver@outlook.jp>
2025-04-25 10:57:56 +0900

    wip: loki

--- middlewares/init.go
+++ middlewares/init.go
@@ -1,16 +1,16 @@
 package middlewares
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func Init(e *echo.Echo) {
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format:           `${time_rfc3339} [${method}]: ${status} {"uri":"${uri}","latency":"${latency}","remote_ip":"${remote_ip}","user_agent":"${user_agent}"}`,
-		CustomTimeFormat: "2006-01-02 15:04:05",
-	}))
 	e.Use(middleware.Recover())
 	e.Use(middleware.Gzip())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
@@ -20,4 +20,15 @@ func Init(e *echo.Echo) {
 	e.Use(middleware.Secure())
 	e.Use(MySQLConnectMiddleware())
 	e.Use(otelecho.Middleware("api-server"))
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format:           `${time_rfc3339} [${method}]: ${status} {"uri":"${uri}","latency":"${latency}","remote_ip":"${remote_ip}","user_agent":"${user_agent}", "${custom}"}`,
+		CustomTimeFormat: "2006-01-02 15:04:05",
+		CustomTagFunc: func(c echo.Context, buf *bytes.Buffer) (int, error) {
+			span := trace.SpanFromContext(c.Request().Context())
+			buf.WriteString(fmt.Sprintf("traceID: %s, spanId: %s", span.SpanContext().TraceID().String(), span.SpanContext().SpanID().String()))
+
+			return 0, nil
+		},
+	}))
+
 }
